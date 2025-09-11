e7436df5f5e707634a4859c38f25026613ce8bf8
Xtremiz <fozanahmed01@gmail.com>
2025-09-11 20:46:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('punkt_tab')
 df = pd.read_csv('spam.csv', encoding='latin1')
 df = df[['v1','v2']]
 df.rename(columns={"v1":"status","v2":"Text"},inplace=True)
@@ -10,4 +12,11 @@ df.rename(columns={"v1":"status","v2":"Text"},inplace=True)
 
 le = LabelEncoder()
 df['status'] = le.fit_transform(df['status'])
-print(df['status'].value_counts())
+count = df['status'].value_counts()
+
+df['num_character'] = df['Text'].apply(len)
+
+
+df['num_word'] = df['Text'].apply(lambda x:nltk.word_tokenize(x))
+print(df.head())
+print(df.info())
