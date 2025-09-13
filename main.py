29dd1c4b34271a86b73dc6f05034f6be857ae5dc
Xtremiz <fozanahmed01@gmail.com>
2025-09-14 01:12:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
 import seaborn as sns
 import matplotlib.pyplot as plt
 import nltk
 import seaborn as sns
 import Extra as e
-from collections import Counter
-from wordcloud import WordCloud
+from sklearn.model_selection import train_test_split
+cv = CountVectorizer()
 df = pd.read_csv('spam.csv', encoding='latin1')
 df = df[['v1', 'v2']]
 df.rename(columns={"v1": "status", "v2": "Text"}, inplace=True)
@@ -26,17 +25,9 @@ df['num_sentence'] = df['Text'].apply(lambda x: len(nltk.sent_tokenize(x)))
 
 df['Transform Text'] = df['Text'].apply(e.transform_text)
 
-wc = WordCloud(width=500, height=500, min_font_size=10,
-               background_color='white')
-df['Transform Text'] = df['Transform Text'].apply(lambda x: str(x))
+X = cv.fit_transform(df['Transform Text']).toarray()
+y= df['status']
 
-list_of_text = df[df['status'] == 1]['Transform Text'].to_list()
-corpus = []
-for i in list_of_text:
-    for j in i.split():
-        corpus.append(i)
-df = pd.DataFrame(Counter(corpus).most_common(30))
-sns.barplot(x=0, y=1, data=df)
-plt.xticks(rotation='vertical')
-plt.show()
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+print(X_train.head())
 
